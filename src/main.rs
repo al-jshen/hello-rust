cb07f99471270120a2ee0e4fb35e8f1257aeb645
Jeff Shen <jshen2014@hotmail.com>
2020-06-19 17:29:45 -0400

    selectively responding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpStream, TcpListener};
 
 fn main() {
-    let listener = TcpListener::bind("localhost:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
@@ -13,7 +14,21 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        //println!("{}", String::from_utf8_lossy(&buffer[..]));
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
