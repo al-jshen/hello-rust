f9034e6f29c4998d067cd3ca307758972c5fc5c5
Jeff Shen <jshen2014@hotmail.com>
2020-06-19 19:14:16 -0400

    multithreading: pool, workers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
-pub struct ThreadPool;
+use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Job;
 
 pub struct PoolCreationError {
     message: String,
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker { id, thread }
+    }
 }
 
 impl ThreadPool {
@@ -14,7 +36,17 @@ impl ThreadPool {
     // `new` will panic if the size is 0.
     pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
         if size > 0 {
-            Ok(ThreadPool)
+            let (sender, receiver) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+
+            let mut workers = Vec::with_capacity(size);
+
+            for id in 0..size {
+                let worker = Worker::new(id, Arc::clone(&receiver));
+                workers.push(worker);
+            }
+
+            Ok(ThreadPool { workers, sender })
         } else {
             Err(PoolCreationError { message: "size must be greater than 0".to_string() })
         }
