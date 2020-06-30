627df87fbf5cd6d6453106ec18f643246ebf9ba2
Jeff Shen <jshen2014@hotmail.com>
2020-06-30 09:19:55 -0400

    more threads, removed testing code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,9 +7,9 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4).unwrap();
+    let pool = ThreadPool::new(12).unwrap();
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(stream);
